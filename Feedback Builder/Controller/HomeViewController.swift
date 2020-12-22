60c769ea87a8c562e59432095af0520dcee90f91
Utkarsh Varma <uvroxx2323@gmail.com>
2020-12-22 14:06:41 -0500

    Finalized the backend and networking for the quote module.

--- Feedback Builder/Controller/HomeViewController.swift
+++ Feedback Builder/Controller/HomeViewController.swift
@@ -13,13 +13,31 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var weatherTemp: UILabel!
     @IBOutlet weak var quoteOfTheDayContent: UILabel!
     @IBOutlet weak var todaysDate: UILabel!
+    
+    var quoteManager = QuoteManager()
     let dateAndTime = DateAndTime()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         todaysDate.text = dateAndTime.getDisplayDate()
+        quoteManager.getQuoteDelegate = self
+        quoteManager.getDailyQuotes()
     }
     
 
    
 
+}
+
+extension HomeViewController:GetQuoteDelegate{
+    func todaysQuote(quote: String) {
+        DispatchQueue.main.async {
+            self.quoteOfTheDayContent.text = quote
+        }
+       
+    }
+    
+    
+    
+    
 }
---
+++ Feedback Builder/Model/WeatherManager.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherManager.swift
+//  Feedback Builder
+//
+//  Created by UTKARSH VARMA on 2020-12-22.
+//
+
+import Foundation
+
+struct WeatherManager{
+    
+}
