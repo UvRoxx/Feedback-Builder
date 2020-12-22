bc52f4a1ec6b3918d2be13cb9519bf0119855c0a
Utkarsh Varma <uvroxx2323@gmail.com>
2020-12-22 00:39:15 -0500

    Added the quote field and weather field to the dash board so the user will get the default weather and a quote of the day for his or her location

--- Feedback Builder/AppDelegate.swift
+++ Feedback Builder/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         //Changing the UITabBarColor
         UITabBar.appearance().barTintColor = .black
-        UITabBar.appearance().tintColor = .green
+        UITabBar.appearance().tintColor = .systemTeal
         return true
     }
 
